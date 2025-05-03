6723bf76dc5200ac1340849b94339acab286ecc7
ChoiMari <chl98639863@gmail.com>
2025-05-03 22:51:06 +0900

    햄버거 메뉴

--- WinCalcPro/WinCalcPro/Form1.Designer.cs
+++ WinCalcPro/WinCalcPro/Form1.Designer.cs
@@ -29,21 +29,48 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
+            this.panel_menu = new System.Windows.Forms.Panel();
+            this.button_close = new System.Windows.Forms.Button();
+            this.button_menu = new System.Windows.Forms.Button();
+            this.panel_menu.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // panel_menu
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(249)))));
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(13, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(35, 35);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = false;
+            this.panel_menu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(248)))), ((int)(((byte)(252)))));
+            this.panel_menu.Controls.Add(this.button_close);
+            this.panel_menu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel_menu.Location = new System.Drawing.Point(0, 0);
+            this.panel_menu.Name = "panel_menu";
+            this.panel_menu.Size = new System.Drawing.Size(0, 501);
+            this.panel_menu.TabIndex = 1;
+            // 
+            // button_close
+            // 
+            this.button_close.FlatAppearance.BorderSize = 0;
+            this.button_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_close.Image = global::WinCalcPro.Properties.Resources.menuImg;
+            this.button_close.Location = new System.Drawing.Point(13, 13);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(35, 35);
+            this.button_close.TabIndex = 0;
+            this.button_close.UseVisualStyleBackColor = true;
+            this.button_close.Visible = false;
+            this.button_close.Click += new System.EventHandler(this.button_close_Click);
+            // 
+            // button_menu
+            // 
+            this.button_menu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(249)))));
+            this.button_menu.FlatAppearance.BorderSize = 0;
+            this.button_menu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_menu.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_menu.Image = global::WinCalcPro.Properties.Resources.menuImg;
+            this.button_menu.Location = new System.Drawing.Point(13, 13);
+            this.button_menu.Name = "button_menu";
+            this.button_menu.Size = new System.Drawing.Size(35, 35);
+            this.button_menu.TabIndex = 0;
+            this.button_menu.UseVisualStyleBackColor = false;
+            this.button_menu.Click += new System.EventHandler(this.button_menu_Click);
             // 
             // Form1
             // 
@@ -51,16 +78,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(249)))));
             this.ClientSize = new System.Drawing.Size(324, 501);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel_menu);
+            this.Controls.Add(this.button_menu);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "계산기";
+            this.panel_menu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_menu;
+        private System.Windows.Forms.Panel panel_menu;
+        private System.Windows.Forms.Button button_close;
     }
 }
--- WinCalcPro/WinCalcPro/Form1.cs
+++ WinCalcPro/WinCalcPro/Form1.cs
@@ -16,5 +16,19 @@ namespace WinCalcPro
         {
             InitializeComponent();
         }
+
+        private void button_menu_Click(object sender, EventArgs e)
+        {
+            panel_menu.Width = 200; // 너비 설정 (필요한 만큼 조절)
+            button_close.Visible = true; // 닫기 버튼 보이게
+            button_menu.Visible = false; // 햄버거 버튼은 숨기기
+        }
+
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            panel_menu.Width = 0; // 다시 닫기
+            button_close.Visible = false;
+            button_menu.Visible = true;
+        }
     }
 }
