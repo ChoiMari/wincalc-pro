943e418cd3b792727a3945ad15f80a303cb6066b
r2d2c2 <uy6806@naver.com>
2025-05-04 15:50:51 +0900

    프로그래머 계산기 디자인 추가

--- WinCalcPro/WinCalcPro/ProgrammerCalc.cs
+++ WinCalcPro/WinCalcPro/ProgrammerCalc.cs
@@ -8,5 +8,30 @@ namespace WinCalcPro
 {
     internal class ProgrammerCalc
     {
+        /// <summary>
+        /// <<
+        /// </summary>
+        /// 
+        public long Left(long value)
+        {
+            return value << 1;
+        }
+        /// <summary>
+        /// >>
+        /// </summary>
+        /// 
+        public long Right(long value)
+        {
+            return value >> 1;
+        }
+
+        ///<summary>
+        ///% 
+        /// </summary>
+        /// 
+        public decimal Percent(decimal value)
+        {
+            return value / 100;
+        }
     }
 }
